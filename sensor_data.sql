7e1a7b7ddba2d38aa9c2a4a33c69fb6d190ebc2d
watggwp <watggwp@gmail.com>
2025-04-02 20:58:15 +0700

    update

---
+++ sensor_data.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 02, 2025 at 03:57 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sensor_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sensor_data`
+--
+
+CREATE TABLE `sensor_data` (
+  `id` int(11) NOT NULL,
+  `temperature` int(11) NOT NULL,
+  `humidity` int(11) NOT NULL,
+  `light` int(11) NOT NULL,
+  `timestamp` datetime DEFAULT current_timestamp(),
+  `greenhouse_id` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `sensor_data`
+--
+
+INSERT INTO `sensor_data` (`id`, `temperature`, `humidity`, `light`, `timestamp`, `greenhouse_id`) VALUES
+(70, 21, 53, 69, '2025-04-02 20:24:04', 1),
+(71, 21, 53, 69, '2025-04-02 20:27:35', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `sensor_data`
+--
+ALTER TABLE `sensor_data`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `sensor_data`
+--
+ALTER TABLE `sensor_data`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=72;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
